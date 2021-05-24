d8e325ae0500c8c03dc85c9d1acc8838e7edd639
EunJin Lee <sue9897@naver.com>
2021-05-24 18:54:39 +0900

    EDIT Metric.py : 'item' to 'movie'

--- Metric.py
+++ Metric.py
@@ -9,32 +9,32 @@ class Metric:
     def get_hits(self, k_ranked, holdout):
         """
         hit 생성 함수
-        hit := holdout(df_test의 item)이 K순위 내에 있는지 여부
+        hit := holdout(df_test의 movie)이 K순위 내에 있는지 여부
         """
-        for item in k_ranked:
-            if item == holdout:
+        for movie in k_ranked:
+            if movie == holdout:
                 return 1
         return 0
 
     def eval_rating(self, idx, test_ratings, test_negatives, K, model):
         """
-        holdout(df_test의 item)이 K순위 내에 있는지 평가하는 함수
+        holdout(df_test의 movie)이 K순위 내에 있는지 평가하는 함수
         """
-        items = test_negatives[idx]      # negative items [neg_item_id, ... ] (1,100)
-        user_idx = test_ratings[idx][0]  # [user_id, item_id][0]
-        holdout = test_ratings[idx][1]   # [user_id, item_id][1]
-        items.append(holdout)            # holdout 추가 [neg_item_id, ..., holdout] (1,101)
+        movies = test_negatives[idx]      # negative movies [neg_movie_id, ... ] (1,100)
+        user_idx = test_ratings[idx][0]  # [user_id, movie_id][0]
+        holdout = test_ratings[idx][1]   # [user_id, movie_id][1]
+        movies.append(holdout)            # holdout 추가 [neg_movie_id, ..., holdout] (1,101)
 
         # prediction
-        predict_user = np.full(len(items), user_idx, dtype='int32').reshape(-1, 1)  # [[user_id], ...], (101, 1)
-        np_items = np.array(items).reshape(-1, 1)                                   # [[item_id], ... ], (101, 1)
+        predict_user = np.full(len(movies), user_idx, dtype='int32').reshape(-1, 1)  # [[user_id], ...], (101, 1)
+        np_movies = np.array(movies).reshape(-1, 1)                                   # [[movie_id], ... ], (101, 1)
 
-        predictions = model.predict([predict_user, np_items])
+        predictions = model.predict([predict_user, np_movies])
         predictions = predictions.flatten().tolist()
-        item_to_pre_score = {item:pre for item, pre in zip(items, predictions)}
+        movie_to_pre_score = {movie:pre for movie, pre in zip(movies, predictions)}
 
         # 점수가 높은 상위 k개 아이템 리스트
-        k_ranked = heapq.nlargest(K, item_to_pre_score, key=item_to_pre_score.get)
+        k_ranked = heapq.nlargest(K, movie_to_pre_score, key=movie_to_pre_score.get)
 
         # holdout이 상위 K 순위에 포함 되는지 체크
         # {1:포함, 0:포함x}
@@ -48,11 +48,11 @@ class Metric:
         """
         hits = []
         test_u = df_test['user_id'].values.tolist()
-        test_i = df_test['item_id'].values.tolist()
+        test_i = df_test['movie_id'].values.tolist()
 
         test_ratings = list(zip(test_u, test_i))
         df_neg = df_neg.drop(df_neg.columns[0], axis=1)
-        test_negatives = df_neg.values.tolist()  # [[(user_id, item_id=holdout)], neg_item,... ] (1,100)
+        test_negatives = df_neg.values.tolist()  # [[(user_id, movie_id=holdout)], neg_movie,... ] (1,100)
 
         # user 샘플링
         sample_idx_lst = np.random.choice(len(test_ratings), int(len(test_ratings) * 0.3))
