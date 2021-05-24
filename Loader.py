053180b78743922432f4f727cdf6974d8445670d
EunJin Lee <sue9897@naver.com>
2021-05-24 18:42:57 +0900

    EDIT Loader.py (MovieLens Dataset)

--- Loader.py
+++ Loader.py
@@ -19,44 +19,45 @@ class Loader():
         df_test
         """
         # 데이터 로드
-        df = pd.read_csv(file_path + '/usersha1-artmbid-artname-plays.tsv', delimiter='\t', header=None)
+        file_path=''
+        df = pd.read_csv(file_path + '/ratings.csv', header=None)
         df = df.drop(df.columns[2], axis=1)
-        df.columns = ['user', 'item', 'plays']
+        df.columns = ['userId', 'movieId', 'rating', 'timestamp']
         df = df.dropna()
         df = df.loc[df.plays != 0]
 
         # user 샘플링
         sample_num = 100000
-        unique_user_lst = list(np.unique(df['user']))  # 358857명
+        unique_user_lst = list(np.unique(df['userId']))  # 358857명
         sample_user_idx = np.random.choice(len(unique_user_lst), sample_num, replace=False)
         sample_user_lst = [unique_user_lst[idx] for idx in sample_user_idx]
-        df = df[df['user'].isin(sample_user_lst)]
+        df = df[df['userId'].isin(sample_user_lst)]
         df = df.reset_index(drop=True)
 
         # 1명 이상의 artist 데이터가 있는 user 만 사용
-        df_count = df.groupby(['user']).count()
-        df['count'] = df.groupby('user')['user'].transform('count')
+        df_count = df.groupby(['userId']).count()
+        df['count'] = df.groupby('userId')['userId'].transform('count')
         df = df[df['count'] > 1]
 
         # user, item 아이디 부여
-        df['user_id'] = df['user'].astype("category").cat.codes
-        df['item_id'] = df['item'].astype("category").cat.codes
+        df['user_id'] = df['userId'].astype("category").cat.codes
+        df['movie_id'] = df['movieId'].astype("category").cat.codes
 
         # lookup 테이블 생성
-        item_lookup = df[['item_id', 'item']].drop_duplicates()
-        item_lookup['item_id'] = item_lookup.item_id.astype(str)
+        item_lookup = df[['movie_id', 'movieId']].drop_duplicates()
+        item_lookup['movie_id'] = item_lookup.movie_id.astype(str)
 
         # train, test 데이터 생성
-        df = df[['user_id', 'item_id', 'plays']]
+        df = df[['user_id', 'movie_id', 'rating', 'timestamp']]
         df_train, df_test = self.train_test_split(df)
 
         # 전체 user, item 리스트 생성
         users = list(np.sort(df.user_id.unique()))
-        items = list(np.sort(df.item_id.unique()))
+        items = list(np.sort(df.movie_id.unique()))
 
         # train user, item 리스트 생성
         rows = df_train['user_id'].astype(int)
-        cols = df_train['item_id'].astype(int)
+        cols = df_train['movie_id'].astype(int)
         values = list(df_train.plays)
 
         uids = np.array(rows.tolist())
@@ -73,7 +74,7 @@ class Loader():
         """
         negativeList = []
         test_u = df_test['user_id'].values.tolist()
-        test_i = df_test['item_id'].values.tolist()
+        test_i = df_test['movie_id'].values.tolist()
 
         test_ratings = list(zip(test_u, test_i))  # test (user, item)세트
         zipped = set(zip(uids, iids))             # train (user, item)세트
@@ -108,10 +109,10 @@ class Loader():
         df_train = df.copy(deep=True)
 
         # df_test
-        # user_id와 holdout_item_id(user가 플레이한 아이템 중 1개)뽑기
+        # user_id와 holdout_movie_id(user가 플레이한 아이템 중 1개)뽑기
         df_test = df_test.groupby(['user_id']).first()
         df_test['user_id'] = df_test.index
-        df_test = df_test[['user_id', 'item_id', 'plays']]
+        df_test = df_test[['user_id', 'movie_id', 'rating', 'timestamp']]
         df_test = df_test.reset_index(drop=True)
 
         # df_train
@@ -125,14 +126,14 @@ class Loader():
         """
         모델에 사용할 train 데이터 생성 함수
         """
-        user_input, item_input, labels = [],[],[]
+        user_input, movie_input, labels = [],[],[]
         zipped = set(zip(uids, iids)) # train (user, item) 세트
 
         for (u, i) in zip(uids, iids):
 
             # pos item 추가
             user_input.append(u)  # [u]
-            item_input.append(i)  # [pos_i]
+            movie_input.append(i)  # [pos_i]
             labels.append(1)      # [1]
 
             # neg item 추가
@@ -143,7 +144,7 @@ class Loader():
                     j = np.random.randint(num_items)  # 다시 샘플링
 
                 user_input.append(u)  # [u1, u1,  u1,  ...]
-                item_input.append(j)  # [pos_i, neg_j1, neg_j2, ...]
+                movie_input.append(j)  # [pos_i, neg_j1, neg_j2, ...]
                 labels.append(0)      # [1, 0,  0,  ...]
 
-        return user_input, item_input, labels
+        return user_input, movie_input, labels
