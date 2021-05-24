839ac5e3e9553d9df04473732974a830b1dfb711
EunJin Lee <sue9897@naver.com>
2021-05-24 18:49:44 +0900

    EDIT Run.py : 'item' to 'movie & 'iids' to 'mids'

--- Run.py
+++ Run.py
@@ -14,23 +14,23 @@ class Run:
         print('start data load..')
 
         num_neg = 4
-        uids, iids, self.df_train, self.df_test, \
-        self.df_neg, self.users, self.items, item_lookup = loader.load_dataset()
-        user_input, item_input, labels = loader.get_train_instances(uids, iids, num_neg, len(self.items))
+        uids, mids, self.df_train, self.df_test, \
+        self.df_neg, self.users, self.movies, movie_lookup = loader.load_dataset()
+        user_input, movie_input, labels = loader.get_train_instances(uids, mids, num_neg, len(self.movies))
 
         print('end data load..')
 
         # input data 준비
-        user_data_shuff, item_data_shuff, label_data_shuff = shuffle(user_input, item_input, labels)
+        user_data_shuff, movie_data_shuff, label_data_shuff = shuffle(user_input, movie_input, labels)
         self.user_data_shuff = np.array(user_data_shuff).reshape(-1,1)
-        self.item_data_shuff = np.array(item_data_shuff).reshape(-1,1)
+        self.movie_data_shuff = np.array(movie_data_shuff).reshape(-1,1)
         self.label_data_shuff = np.array(label_data_shuff).reshape(-1,1)
 
     def run(self):
 
-        nmf = NeuMF(len(self.users), len(self.items))  # Neural Collaborative Filtering
+        nmf = NeuMF(len(self.users), len(self.movies))  # Neural Collaborative Filtering
         self.model = nmf.get_model()
-        self.model.fit([self.user_data_shuff, self.item_data_shuff], self.label_data_shuff, epochs=20,
+        self.model.fit([self.user_data_shuff, self.movie_data_shuff], self.label_data_shuff, epochs=20,
                        batch_size=256, verbose=1)
 
         return self.model
@@ -53,13 +53,13 @@ if __name__ == '__main__':
 
     # user 한 명에 대한 prediction 예시
     user_id = 0
-    user_candidate_item = np.array([134, 6783, 27888, 8362, 25]).reshape(-1, 1)
-    user_input = np.full(len(user_candidate_item), user_id, dtype='int32').reshape(-1, 1)
+    user_candidate_movie = np.array([134, 6783, 27888, 8362, 25]).reshape(-1, 1)
+    user_input = np.full(len(user_candidate_movie), user_id, dtype='int32').reshape(-1, 1)
 
-    predictions = model.predict([user_input, user_candidate_item])
+    predictions = model.predict([user_input, user_candidate_movie])
     predictions = predictions.flatten().tolist()
-    item_to_pre_score = {item[0]: pre for item, pre in zip(user_candidate_item, predictions)}  # 후보 아이템 별 예측값
-    item_to_pre_score = dict(sorted(item_to_pre_score.items(), key=lambda x: x[1], reverse=True))
+    movie_to_pre_score = {movie[0]: pre for movie, pre in zip(user_candidate_movie, predictions)}  # 후보 아이템 별 예측값
+    movie_to_pre_score = dict(sorted(movie_to_pre_score.movies(), key=lambda x: x[1], reverse=True))
 
-    recommend_item_lst = list(item_to_pre_score.keys())
-    print('recommend:', recommend_item_lst)
+    recommend_movie_lst = list(movie_to_pre_score.keys())
+    print('recommend:', recommend_movie_lst)
